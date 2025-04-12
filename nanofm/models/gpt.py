4b61248cbd79a4c05709701c65a791dd5f1ab076
usercharly123 <charlesedouard.rouault@gmail.com>
2025-04-12 15:17:28 +0200

    up loss

--- nanofm/models/gpt.py
+++ nanofm/models/gpt.py
@@ -156,16 +156,16 @@ class GPT(nn.Module):
         """
         # TODO: Compute the cross-entropy loss
         # Hint: Remember to ignore the padding token index in the loss calculation
-        loss = F.cross_entropy(
+        """ loss = F.cross_entropy(
             logits.view(-1, logits.size(-1)),
             target_seq.view(-1),
             ignore_index=padding_idx
-        )
-        """ B, L, vocab_size = logits.size()
+        ) """
+        B, L, vocab_size = logits.size()
         if padding_idx is not None:
             loss = F.cross_entropy(logits.reshape(B*L, vocab_size), target_seq.reshape(B*L), ignore_index=padding_idx)
         else:
-            loss = F.cross_entropy(logits.reshape(B*L, vocab_size), target_seq.reshape(B*L)) """
+            loss = F.cross_entropy(logits.reshape(B*L, vocab_size), target_seq.reshape(B*L))
         return loss
 
     def forward(self, data_dict: Dict[str, Any]) -> Dict[str, Any]:
