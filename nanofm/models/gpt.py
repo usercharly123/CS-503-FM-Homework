12d2687943064fe478d6fda732590b601394fcd1
usercharly123 <charlesedouard.rouault@gmail.com>
2025-04-15 22:20:05 +0200

    test ones

--- nanofm/models/gpt.py
+++ nanofm/models/gpt.py
@@ -124,7 +124,9 @@ class GPT(nn.Module):
         # False = masked-out, True = not masked. Shape: [1, L, L]
         # Hint: What shape should the mask have such that each token can attend to itself and
         # all previous tokens, but not to any future tokens?
-        mask = torch.triu(torch.ones((1, L, L), device=self.device), diagonal=-1).bool()     # diag=0 to include the diagonal (self-attention)
+        mask = torch.tril(torch.ones((1, L, L), device=self.device), diagonal=0).bool()     # diag=0 to include the diagonal (self-attention)
+        # test mask = ones
+        mask = torch.ones((1, L, L), device=self.device).bool()
             
         # TODO: Forward pass through Transformer trunk
         # Hint: Make sure to pass the causal mask to the transformer trunk too
