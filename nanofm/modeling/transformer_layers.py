bd1ce70ab663edb7729354336388c1857b8540c9
usercharly123 <charlesedouard.rouault@gmail.com>
2025-04-15 23:15:56 +0200

    up clean

--- nanofm/modeling/transformer_layers.py
+++ nanofm/modeling/transformer_layers.py
@@ -62,15 +62,10 @@ class Mlp(nn.Module):
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         
-        # TODO
-        # Define the first linear layer with GELU activation
         self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
-        # Define the second linear layer
         self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-
-        # TODO
         x = self.fc1(x)
         x = F.gelu(x)
         x = self.fc2(x)
@@ -94,10 +89,8 @@ class Attention(nn.Module):
 
         # TODO: Define here the linear layer(s) producing K, Q, V from the input x
         # Hint: Do you need to define three different projections, or can you use a single one for all three?
-        self.qkv = nn.Linear(dim, 3*head_dim*self.num_heads, bias=qkv_bias)
-        """ self.q = nn.Linear(dim, head_dim*self.num_heads, bias=qkv_bias)
-        self.k = nn.Linear(dim, head_dim*self.num_heads, bias=qkv_bias)
-        self.v = nn.Linear(dim, head_dim*self.num_heads, bias=qkv_bias) """
+        self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
+        self.head_dim = head_dim
 
         self.attn_out_proj = nn.Linear(dim, dim, bias=proj_bias)
 
@@ -105,30 +98,28 @@ class Attention(nn.Module):
         B, L, D = x.shape # Batch size, sequence length, and dimension
 
         # TODO: Compute the keys K, queries Q, and values V from x. Each should be of shape [B num_heads L head_dim].
-        qkv = rearrange(self.qkv(x), "B L (three head_dim num_heads) -> three B num_heads L head_dim", num_heads=self.num_heads, three=3)
-        q = qkv[0] # [B num_heads L head_dim]
-        k = qkv[1] # [B num_heads L head_dim]   
-        v = qkv[2] # [B num_heads L head_dim]   
-        """ q = rearrange(self.q(x), "B L (head_dim num_heads) -> B num_heads L head_dim", num_heads=self.num_heads)
-        k = rearrange(self.k(x), "B L (head_dim num_heads) -> B num_heads L head_dim", num_heads=self.num_heads)
-        v = rearrange(self.v(x), "B L (head_dim num_heads) -> B num_heads L head_dim", num_heads=self.num_heads) """
+        q, k, v = self.qkv(x).chunk(3, dim=-1) # Split the output into Q, K, V
+        q = rearrange(q, "B L (num_heads head_dim) -> B num_heads L head_dim", num_heads=self.num_heads, head_dim=self.head_dim)
+        k = rearrange(k, "B L (num_heads head_dim) -> B num_heads L head_dim", num_heads=self.num_heads, head_dim=self.head_dim)
+        v = rearrange(v, "B L (num_heads head_dim) -> B num_heads L head_dim", num_heads=self.num_heads, head_dim=self.head_dim)
 
         # TODO: Compute the attention matrix (pre softmax) and scale it by 1/sqrt(d_k). It should be of shape [B num_heads L L].
         # Hint: Use the already defined self.scale
-        attn = self.scale * (q @ k.transpose(-2, -1)) # invert the last two dimensions of k to get [B num_heads head_dim L]
+        attn = torch.einsum("b h i d, b h j d -> b h i j", q, k) * self.scale # [B num_heads L L]
 
         if mask is not None:
             mask = rearrange(mask, "b n m -> b 1 n m") # Unsqueeze for multi-head attention
             # TODO: Apply the optional attention mask. Wherever the mask is False, replace the attention 
             # matrix value by negative infinity → zero attention weight after softmax.
-            attn = attn.masked_fill(~mask, float("-inf"))
+            attn = attn.masked_fill(mask == 0, float("-inf"))
 
         # TODO: Compute the softmax over the last dimension
         attn = attn.softmax(dim=-1)
 
         # TODO: Weight the values V by the attention matrix and concatenate the different attention heads
         # Make sure to reshape the output to the original shape of x, i.e. [B L D]
-        x = (attn @ v).reshape(B, L, D)
+        x = torch.einsum("b h i j, b h j d -> b h i d", attn, v) # [B num_heads L head_dim]
+        x = rearrange(x, "b h i d -> b i (h d)", h=self.num_heads) # [B L D]
 
         # Output projection
         x = self.attn_out_proj(x)
@@ -148,16 +139,17 @@ class Block(nn.Module):
     def __init__(self, dim: int, head_dim: int = 64, mlp_ratio: float = 4., use_bias: bool = False):
         super().__init__()
         self.norm1 = LayerNorm(dim) # TODO (use the LayerNorm defined above)
-        self.attn = Attention(dim, head_dim=head_dim, qkv_bias=use_bias, proj_bias=use_bias) # TODO
+        self.attn = Attention(dim, head_dim, qkv_bias=use_bias, proj_bias=use_bias)
         self.norm2 = LayerNorm(dim) # TODO (use the LayerNorm defined above)
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(dim, mlp_hidden_dim, dim, bias=use_bias) # TODO
+        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, out_features=dim, bias=use_bias)
 
     def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         
-        # TODO
-        x = x + self.attn(self.norm1(x), mask=mask)
-        x = x + self.mlp(self.norm2(x))
+        x = self.norm1(x)
+        x = self.attn(x, mask=mask) + x # Residual connection
+        x = self.norm2(x)
+        x = self.mlp(x) + x # Residual connection
         return x
 
 
@@ -182,12 +174,12 @@ class TransformerTrunk(nn.Module):
         ):
         super().__init__()
 
-        self.blocks = nn.ModuleList([Block(dim, head_dim, mlp_ratio, use_bias) for i in range(depth)]) # TODO: Create a list of transformer blocks and wrap inside nn.ModuleList
-    
+        self.blocks = nn.ModuleList([
+            Block(dim, head_dim=head_dim, mlp_ratio=mlp_ratio, use_bias=use_bias) for _ in range(depth)
+        ])
+        
     def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         
-        # TODO
         for block in self.blocks:
             x = block(x, mask=mask)
         return x
-
